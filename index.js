eb7b09b9c94616e47a3d3a31ace18a880702587c
Eash Khatri <eash.khatri@evontech.com>
2019-04-04 12:11:22 +0530

    push

--- index.js
+++ index.js
@@ -16,19 +16,11 @@ function getSAMLResponse() {
         AuthnRequest: {
             '@xmlns': XMLNS.SAMLP,
             '@xmlns:saml': XMLNS.SAML,
-            '@Version': '2.0',
-            '@ID': id,
+            '@Version': '1.0',
             '@IssueInstant': (new Date()).toISOString(),
-            '@Destination': destination,
-            '@AssertionConsumerServiceURL': assert_endpoint,
-            '@ProtocolBinding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-            '@ForceAuthn': force_authn,
-            'saml:Issuer': issuer,
             NameIDPolicy: {
-                '@Format': nameid_format || 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
                 '@AllowCreate': 'true'
-            },
-            RequestedAuthnContext: context_element
+            }
         }
     }).end();
 }
